5bd7cb78ad797951eb7d8034410fd36d7a06481f
Angadi <angadi@8c8590430112.ant.amazon.com>
2019-08-22 15:40:59 +0530

    code-deploy-hello-world-rev3

--- stopserver.sh
+++ stopserver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 isExistHttpd = `pgrep httpd`
-isExistMysqld = `pgrep mysqld`
+#isExistMysqld = `pgrep mysqld`
 if [[ -n  $isExistHttpd ]]; then
     service httpd stop        
 fi
-if [[ -n $isExistMysqld ]]; then
-	service mysqld stop
-fi
+#if [[ -n $isExistMysqld ]]; then
+#	service mysqld stop
+#fi
